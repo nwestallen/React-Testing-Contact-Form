b8229f4b7f7f26b8ec094fed98fa64aff17019b3
Nick Allen <nwestallen@gmail.com>
2021-02-11 15:44:33 -0500

    Made ContactForm.test.js

---
+++ src/ContactForm.test.js
@@ -0,0 +1,22 @@
+import '@testing-library/jest-dom/extend-expect';
+import 'mutationobserver-shim';
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+import ContactForm from './components/ContactForm';
+import React from 'react';
+
+test('Form renders', () => {
+    render(<ContactForm />);
+});
+
+test('All form fields fill', () => {
+    render(<ContactForm />);
+
+    //const firstNameInput = screen.getByLabelText(/first name*/i);
+    const lastNameInput = screen.getByLabelText(/last name*/i);
+    //const emailInput = screen.getByLabelText(/email*/i);
+    const messageInput = screen.getByLabelText(/message*/i);
+
+    //userEvent.type(firstNameInput, "Hi");
+    userEvent.type(lastNameInput, "Allen");
+})
